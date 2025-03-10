9263e60edee07ebee46402bd418d41c59c30919b
Rafael Puchades <rpuchadm@cfp.upv.es>
2025-03-10 18:05:01 +0100

    auth_api

--- 10_simple_api_rest/auth_api/src/main.rs
+++ 10_simple_api_rest/auth_api/src/main.rs
@@ -1,10 +1,12 @@
+use rocket::FromForm;
+use rocket::form::Form;
 use rocket::http::Status;
 use chrono::prelude::*;
 use rocket::request::{self, Request, FromRequest};
 use rocket::outcome::Outcome;
 use rocket::serde::{Deserialize, Serialize};
 use rocket::serde::json::Json;
-use rocket::{delete, get, launch, post, put, routes, State};
+use rocket::{ get, launch, post, routes, State}; // delete, put
 use sqlx::{Decode, FromRow};
 
 struct AppState {
@@ -14,6 +16,8 @@ struct AppState {
 #[derive(Serialize, Deserialize, Clone, FromRow, Decode, Debug)]
 struct Session {
     id: i32,
+    //#[serde(skip_serializing)]
+    //code: String,
     token: String,
     user_id: i32,
     created_at: chrono::NaiveDateTime,
@@ -38,17 +42,74 @@ impl<'r> FromRequest<'r> for BearerToken {
     }
 }
 
-#[get("/profile")]
-async fn profile(state: &State<AppState>, token: BearerToken) -> Option<Json<Session>> {
+#[derive(Deserialize, Serialize, FromForm)]
+struct AccessTokenRequest {
+    //grant_type: String,
+    client_id: String,
+    //client_secret: String,
+    //redirect_uri: String,
+    code: String,
+}
+
+// a post accessToken se accede 1 sola vez con el code para obtener el token
+#[post("/accessToken", data = "<request>")]
+async fn access_token(state: &State<AppState>, request: Form<AccessTokenRequest>) -> Result<Json<Session>, Status> {
+    // Aquí puedes procesar los datos del formulario
+    //let grant_type = &request.grant_type;
+    let client_id = &request.client_id;
+    //let client_secret = &request.client_secret;
+    //let redirect_uri = &request.redirect_uri;
+    let code = &request.code;
+
+    print!("access_token client_id: {}, code: {}", client_id, code);
+
     let pool = state.pool.clone();
-    let session = postgres_get_session(&pool, &token.0)
+
+    // Obtiene la sesión por el código de autorización y el id del cliente
+    // si no encuentra la sesión devuelve un error 500
+    let session = postgres_get_session_by_code_client_id(&pool, code, client_id)
         .await
-        .ok()?;
+        .map_err(|err| {
+            eprintln!("Error getting session by code={} and client_id={} : {:?}", code, client_id, err);
+            Status::Forbidden
+        })?;
+
+    // Log para inspeccionar la sesión
+    println!("access_token Session: {:?}", session);
+
+    // Verifica que el código de autorización sea válido
+    if session.token.is_empty() {
+        eprintln!("Error empty token for code={} and client_id={}", code, client_id);
+        return Err(Status::Forbidden);
+    }
+
+    // Actualiza el código de autorización a nulo
+    postgres_update_session_set_code_null_by_id(&pool, session.id)
+        .await
+        .or_else(|err| {
+            eprintln!("Error updating session code to null by id={} : {:?}", session.id, err);
+            Err(err)
+        })
+        .unwrap();
+
+    Ok(Json(session.clone()))
+}
+
+// a get profile se accede de manera reiterada con el token para obtener la sesión
+#[get("/profile")]
+async fn profile(state: &State<AppState>, token: BearerToken) -> Result<Json<Session>, Status> {
+    let pool = state.pool.clone();
+    let session = postgres_get_session_by_token(&pool, &token.0)
+        .await
+        .map_err(|err| {
+            eprintln!("Error getting session by token={} : {:?}", token.0, err);
+            Status::Forbidden
+        })?;
 
     // Log para inspeccionar la sesión
     println!("Session: {:?}", session);
 
-    Some(Json(session.clone()))
+    Ok(Json(session.clone()))
 }
 
 #[launch]
@@ -61,12 +122,24 @@ async fn rocket() -> _ {
     })
     .unwrap();
 
+    // intenta ejecutar DROP TABLE IF EXISTS sessions
+    sqlx::query(
+        r#"
+        DROP TABLE IF EXISTS sessions
+        "#
+    )
+    .execute(&pool)
+    .await
+    .unwrap();
+
     // intenta ejecutar CREATE TABLE IF NOT EXISTS sessions
     sqlx::query(
         r#"
         CREATE TABLE IF NOT EXISTS sessions (
             id SERIAL PRIMARY KEY,
-            token TEXT NOT NULL,
+            code TEXT,
+            client_id TEXT NOT NULL,
+            token TEXT NOT NULL UNIQUE,
             user_id INTEGER NOT NULL,
             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             expires_at TIMESTAMP NOT NULL,
@@ -78,13 +151,33 @@ async fn rocket() -> _ {
     .await
     .unwrap();
 
+    // crea indice para code y client_id cuando code no es nulo
+    sqlx::query(
+        r#"
+        CREATE INDEX IF NOT EXISTS idx_sessions_code_client_id
+        ON sessions (code, client_id)
+        WHERE code IS NOT NULL
+        "#
+    )
+    .execute(&pool)
+    .await
+    .unwrap();
+
     let expires_at: NaiveDateTime = (Utc::now() + chrono::Duration::days(1)).naive_utc();
     // intenta ejecutar INSERT INTO sessions (token, user_id, expires_at, attributes) VALUES ($1, $2, $3, $4)
     sqlx::query(
         r#"
-        INSERT INTO sessions (token, user_id, expires_at, attributes) VALUES ($1, $2, $3, $4)
+        INSERT INTO sessions (
+            code, client_id, token, 
+            user_id, expires_at, attributes
+        ) VALUES (
+            $1, $2, $3,
+            $4, $5, $6
+        )
         "#
     )
+    .bind("mycode")
+    .bind("myclientid")
     .bind("mytoken")
     .bind(1)
     .bind( expires_at)
@@ -97,13 +190,13 @@ async fn rocket() -> _ {
         .manage(AppState {
             pool,
         })
-        .mount("/", routes![profile])
+        .mount("/", routes![profile, access_token])
 }
 
 // constante con el servidor de postgres
 const POSTGRES_SERVER: &str = "postgresql://myuser:mypassword@localhost:5432/mydatabase";
 
-async fn postgres_get_session( pool: &sqlx::Pool<sqlx::Postgres>, token: &str) -> Result<Session, sqlx::Error> {
+async fn postgres_get_session_by_token( pool: &sqlx::Pool<sqlx::Postgres>, token: &str) -> Result<Session, sqlx::Error> {
     let session = sqlx::query_as::<_, Session>(
         r#"
         SELECT id, token, user_id, created_at, expires_at, attributes
@@ -116,4 +209,35 @@ async fn postgres_get_session( pool: &sqlx::Pool<sqlx::Postgres>, token: &str) -
     .await?;
 
     Ok(session)
+}
+
+async fn postgres_get_session_by_code_client_id( pool: &sqlx::Pool<sqlx::Postgres>, code: &str, client_id: &str) -> Result<Session, sqlx::Error> {
+    let session = sqlx::query_as::<_, Session>(
+        r#"
+        SELECT id, token, user_id, created_at, expires_at, attributes
+        FROM sessions
+        WHERE code = $1 and client_id = $2
+        "#
+    )
+    .bind(code)
+    .bind(client_id)
+    .fetch_one(pool)
+    .await?;
+
+    Ok(session)
+}
+
+async fn postgres_update_session_set_code_null_by_id( pool: &sqlx::Pool<sqlx::Postgres>, id: i32) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"
+        UPDATE sessions
+        SET code = NULL
+        WHERE id = $1
+        "#
+    )
+    .bind(id)
+    .execute(pool)
+    .await?;
+
+    Ok(())
 }
